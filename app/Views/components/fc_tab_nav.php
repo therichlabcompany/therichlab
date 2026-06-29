e507018961ed1255fd8a13728c2d8b0db70b4b2e
jungyppil <nik01@medimap.team>
2026-06-29 12:31:48 +0900

    작업본 반영

--- app/Views/components/fc_tab_nav.php
+++ app/Views/components/fc_tab_nav.php
@@ -1,7 +1,7 @@
 <nav class="c-tabs">
-    <a href="javascript:void(0);"<? if($menu_step == "step1") echo ' aria-current="page"' ?>>약관 및 동의</a>
-    <a href="javascript:void(0);"<? if($menu_step == "step2") echo ' aria-current="page"' ?>>기본정보 입력</a>
-    <a href="javascript:void(0);"<? if($menu_step == "step3") echo ' aria-current="page"' ?>>프로필 입력</a>
-    <a href="javascript:void(0);"<? if($menu_step == "step4") echo ' aria-current="page"' ?>>활동정보 입력</a>
-    <a href="javascript:void(0);"<? if($menu_step == "step5") echo ' aria-current="page"' ?>>활동 스토리</a>
+    <a href="javascript:void(0);"<?php if($menu_step == "step1") echo ' aria-current="page"' ?>>약관 및 동의</a>
+    <a href="javascript:void(0);"<?php if($menu_step == "step2") echo ' aria-current="page"' ?>>기본정보 입력</a>
+    <a href="javascript:void(0);"<?php if($menu_step == "step3") echo ' aria-current="page"' ?>>프로필 입력</a>
+    <a href="javascript:void(0);"<?php if($menu_step == "step4") echo ' aria-current="page"' ?>>활동정보 입력</a>
+    <a href="javascript:void(0);"<?php if($menu_step == "step5") echo ' aria-current="page"' ?>>활동 스토리</a>
 </nav>
