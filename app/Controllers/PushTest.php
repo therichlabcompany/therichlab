d72dfc537c7f60d01fd1da67b5b0a26cc123f396
jungyppil <niki01@medi-map.co.kr>
2026-07-28 09:22:48 +0900

    푸쉬 테스트

--- app/Controllers/PushTest.php
+++ app/Controllers/PushTest.php
@@ -8,14 +8,14 @@ class PushTest extends BaseController
 {
     public function index()
     {
-        $deviceToken = 'e5ly-BQvRa6fCAeq6gTxYO:APA91bFGL2-IlDTwxbntLWN7u906Ki3GKP9liKI26MmhMZLcuKisW84sR8GhQSF7USzpIKTNU3lPgNPswc7svN-bbcObj4RsknigvaV5EVcpOHFhcPY6nWc';
+        $deviceToken = 'fp5AZ8kGx0Jdhu83hatwGB:APA91bFv8x27b-40WqzH__rPX2h_Y2vCF5hzZ6mGxP7W_h91tS3gwkwt8CHVy5gHfW42HI7nC0vAGVWYOwNJKHlXWFcEI3U7N2EU2DPfrI23tVgx71hxIoM';
 
         $push = new FirebasePush();
 
         $result = $push->send(
             $deviceToken,
-            'myfc.co.kr 테스트 알림',
-            'myfc.co.kr 보낸 푸시입니다.',
+            'myfc.co.kr 테스트 알림 - ios',
+            'myfc.co.kr 보낸 푸시입니다.-ios',
             [
                 'type' => 'notice',
                 'id'   => 123,
